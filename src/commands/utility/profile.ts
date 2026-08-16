eb4afa05f4388ab0a6195ffc79bed98db5307d31
Octokling(1).js <logangaillard@outlook.fr>
2026-08-16 07:21:02 +0200

    Ajout d'un système d'affichage de profile

--- src/commands/utility/profile.ts
+++ src/commands/utility/profile.ts
@@ -2,58 +2,51 @@ import {
   SlashCommandBuilder,
   EmbedBuilder,
   PermissionFlagsBits,
+  Interaction,
+  ChatInputCommandInteraction,
+  MessageFlags,
+  escapeMarkdown
 } from "discord.js";
+import userProfile from "../../services/user/userProfile.js";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("profile")
-    .setDescription(
-      "Permettre de connaitre les statistique de votre profile ou celle des autres"
-    )
-    .addChannelOption((option) =>
+    .setDescription("Permettre de connaitre les statistiques de votre profile ou celle des autres")
+    .addUserOption((option) =>
       option
-        .setName("salon")
-        .setDescription("Salon où envoyer le message")
-        .setRequired(true)
-    )
+        .setName("utilisateur")
+        .setDescription("Sélectionnez un utilisateur pour accéder à son profile.")
+        .setRequired(false)
+    ),
 
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  async execute(interaction: ChatInputCommandInteraction) {
+    const user = interaction.options.getUser("utilisateur") || interaction.user
 
-  async execute(interaction) {
-    const embed = new EmbedBuilder();
-    embed.setTitle("📹  Retrouvez Romain_roro sur ces réseaux");
-    embed.setDescription(
-      "Suivez-le pour ne pas manquer ses derniers contenus !"
-    );
-    embed.setColor("#ffff00");
-    embed.addFields(
-      {
-        name: "<:Twitch:1441846410801578117> **Twitch**",
-        value: "https://www.twitch.tv/romain_roro__/",
-      },
-      {
-        name: "<:tiktok:1441846401788149892> **TikTok**",
-        value: "https://www.tiktok.com/@romain_roro__/",
-      },
-      {
-        name: "<:instagram:1441846391348396062> **Instagram**",
-        value: "https://www.instagram.com/romain_roro_/",
-      },
-      {
-        name: "<:YouTube:1441846420905791559> **YouTube**",
-        value: "https://www.youtube.com/@Romain_roroo/",
-      }
-    );
-    embed.setFooter({
-      text: "💛 Merci pour votre soutien et à bientôt !",
-    });
-    embed.setThumbnail(
-      "https://yt3.googleusercontent.com/YSxX-rdU2S_WwCtU3Xp1c_lhLAinD3o8Z_YQ8oR51lK9t4tDRzPbmaSciuDPrRhKSiNv8NoUXw=s72-c-k-c0x00ffffff-no-rj"
-    );
+    const profile = userProfile(user, interaction.guildId)
 
-    await interaction.reply({ content: "Message envoyé !", ephemeral: true });
+    if(!profile){
+      return interaction.reply({content:"Aucun utilisateur trouvé", flags: MessageFlags.Ephemeral})
+    }
 
-    const channel = interaction.options.getChannel("salon");
-    channel.send({ embeds: [embed] });
+    const xpInfo = await profile.getUserXP()
+
+    const embed = new EmbedBuilder()
+      .setTitle(`Profile de ${escapeMarkdown(user.globalName)}`)
+      .setThumbnail(user.displayAvatarURL())
+      .addFields(
+        {
+          name: "XP :",
+          value: `${xpInfo.xp}`
+        },
+        {
+          name: "Niveau :",
+          value: `${xpInfo.level}`
+        }
+      )
+      .setFooter({text: "Made with ❤️ by **Octokling**"})
+
+    await interaction.channel.send({embeds: [embed]})
+    await interaction.reply({content: "Profile affiché", flags: MessageFlags.Ephemeral})
   },
 };
